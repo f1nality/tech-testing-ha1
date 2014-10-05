575bca4a999287b5110061a8d375bcc4b25cf1e4
Denis K <f1nal@cgaming.org>
2014-10-05 16:25:17 +0400

    Implement exception on unmocked http requests

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.7
 
 import os
+import socket
 import sys
 import unittest
 
@@ -14,6 +15,21 @@ from tests.test_lib_utils import LibUtilsCase
 from tests.test_lib_worker import LibWorkerTestCase
 
 
+class MockedConnection():
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        self.original_connection = socket.create_connection
+        socket.create_connection = self._create_connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        socket.create_connection = self.original_connection
+
+    @staticmethod
+    def _create_connection(*args, **kwargs):
+        raise AssertionError('Unmocked http request')
+
 if __name__ == '__main__':
     suite = unittest.TestSuite((
         unittest.makeSuite(NotificationPusherTestCase),
@@ -22,5 +38,8 @@ if __name__ == '__main__':
         unittest.makeSuite(LibUtilsCase),
         unittest.makeSuite(LibWorkerTestCase),
     ))
-    result = unittest.TextTestRunner().run(suite)
+
+    with MockedConnection():
+        result = unittest.TextTestRunner().run(suite)
+
     sys.exit(not result.wasSuccessful())
